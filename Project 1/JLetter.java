15d3a1566dd73fb1999d7ce2fd4fb7c70be6a993
Mary <88597516+merrymitch@users.noreply.github.com>
2021-08-07 16:27:12 -0500

    Add files via upload

---
+++ Project 1/JLetter.java
@@ -0,0 +1,26 @@
+/**
+ * Code to print shape of letter J using the word JAVA.
+ *
+ * Project 1
+ * @author Mary Mitchell - COMP1210 - 003
+ * @version 08-28-2020
+ */
+public class JLetter {
+   /**
+    * Prints shape of letter J with the word JAVA.
+    *
+    * @param args Command line arguments - not used.
+    */
+   public static void main(String[] args) {
+      System.out.println("JAVAJAVAJAVA");
+      System.out.println("JAVAJAVAJAVA");
+      System.out.println("      JAVA");
+      System.out.println("      JAVA");
+      System.out.println("      JAVA");
+      System.out.println("      JAVA");
+      System.out.println("J     JAVA");
+      System.out.println("JA    JAVA");
+      System.out.println(" JAVAJAVA");
+      System.out.println("  JAVAJA");
+   }
+}
---
+++ Project 1/MyLifeGoals.java
@@ -0,0 +1,34 @@
+/**
+ * Simple program to print my life goals.
+ * 
+ * Project 1
+ * @author Mary Mitchell - COMP1210 - 003
+ * @version 08-28-2020
+ */
+public class MyLifeGoals {
+   /**
+    * Prints name and short/medium/long term goals.
+    *
+    * @param args Command line arguments - not used.
+    */
+   public static void main(String[] args) {
+      // Print name
+      System.out.println("Mary Mitchell");
+      System.out.println();
+      // Print short-term goals
+      System.out.println("My short-term life goals are to learn how to be a "
+                       + "proficient computer scientist, work on obtaining my "
+                       + "degree in computer science from Auburn, and "
+                       + "hopefully be able to study abroad.");
+      // Print medium-term goals
+      System.out.println("My medium-term life goals are to continue building "
+                       + "a successful career doing what I love, travel the "
+                       + "world a bit more, continue learning, and to be "
+                       + "financially stable and independent.");
+      // Print long-term goals
+      System.out.println("My long-term life goals are to have a career/job "
+                       + "that does something helpful for the world, be "
+                       + "content wherever I am living, and be happy and "
+                       + "healthy with my family.");
+   }
+}
